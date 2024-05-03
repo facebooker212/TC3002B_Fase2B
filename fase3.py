370b07fa079ef2e673f19aa0368c9d98a7f18684
Victor Martinez <42659775+facebooker212@users.noreply.github.com>
2024-05-03 16:12:17 -0600

    Add files via upload

--- fase3.py
+++ fase3.py
@@ -8,6 +8,8 @@ Victor Martinez Roman A01746361
 import os
 import re
 import nltk
+import numpy as np
+import matplotlib.pyplot as plt
 from nltk.tokenize import word_tokenize
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
@@ -81,6 +83,47 @@ def detect_plagiarism(similarity_scores, levenshtein_scores, original_docs, susp
 
     return plagiarism_results
 
+def calculate_auc(actual_results, predicted_results):
+    sorted_indices = np.argsort(predicted_results)[::-1]
+    sorted_actual = [actual_results[i] for i in sorted_indices]
+    sorted_predicted = [predicted_results[i] for i in sorted_indices]
+
+    tpr = []
+    fpr = []
+    num_positive = sum(sorted_actual)
+    num_negative = len(sorted_actual) - num_positive
+    true_positives = 0
+    false_positives = 0
+
+    for i in range(len(sorted_predicted)):
+        if sorted_actual[i]:
+            true_positives += 1
+        else:
+            false_positives += 1
+
+        tpr.append(true_positives / num_positive)
+        fpr.append(false_positives / num_negative)
+
+    auc_value = 0
+    for i in range(1, len(tpr)):
+        auc_value += (tpr[i] - tpr[i-1]) * (fpr[i] + fpr[i-1]) / 2
+
+    return auc_value, fpr, tpr
+
+def plot_roc_curve(actual_results, predicted_results):
+    auc_value, fpr, tpr = calculate_auc(actual_results, predicted_results)
+
+    plt.figure(figsize=(8, 6))
+    plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'ROC curve (AUC = {auc_value:.4f})')
+    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic (ROC) Curve')
+    plt.legend(loc="lower right")
+    plt.show()
+
 def main(original_folder, suspicious_folder):
     original_docs = preprocess_documents(original_folder)
     suspicious_docs = preprocess_documents(suspicious_folder)
@@ -102,6 +145,12 @@ def main(original_folder, suspicious_folder):
     plagiarism_results = detect_plagiarism(similarity_scores, levenshtein_scores, original_docs, suspicious_docs)
 
     # Resultados
+    predicted_results = [1 if result['is_copy'] == 'Si' else 0 for result in plagiarism_results]
+
+    actual_results = [0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0]
+
+    plot_roc_curve(actual_results, predicted_results)
+
     with open("plagiarism_report.txt", "w") as f:
         f.write("Documento sospechoso\tCopia\tDocumento plagiado\t% Plagio\tTipo de plagio\n")
         for result in plagiarism_results:
@@ -118,4 +167,5 @@ if __name__ == "__main__":
     original_folder = "./original/"
     suspicious_folder = "./suspicious_2/"
     main(original_folder, suspicious_folder)
+    #main(original_folder, suspicious_folder, actual_results)
 
